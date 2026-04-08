1cf06448e326934db9a28a7d2f48a23e0d24473a
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-08 21:45:41 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,24 @@
-const resultData = {
-  name: "Muhammed Fawzan",
-  class: "8",
-  roll: "101",
-  subjects: ["Fiqh", "Thareeq", "Duroos", "Lisan"],
-  mark: "350",
-  percentage: "87.5%",
-  rank: "8",
-  attendance: "163"
-};
-
-document.getElementById("name").innerText = resultData.name;
-document.getElementById("class").innerText = resultData.class;
-document.getElementById("roll").innerText = resultData.roll;
-document.getElementById("subjects").innerText = resultData.subjects.join(", ");
-document.getElementById("mark").innerText = resultData.mark;
-document.getElementById("percentage").innerText = resultData.percentage;
-document.getElementById("rank").innerText = resultData.rank;
-document.getElementById("attendance").innerText = resultData.attendance;
+// Student data
+const students = [
+  {
+    name: "Muhammed Fawzan",
+    class: "8",
+    roll: "101",
+    subjects: ["Fiqh", "Thareeq", "Duroos", "Lisan"],
+    mark: "350",
+    percentage: "87.5%",
+    rank: "8",
+    attendance: "163"
+  },
+  {
+    name: "Amina Sulaiman",
+    class: "8",
+    roll: "102",
+    subjects: ["Fiqh", "Thareeq", "Duroos", "Lisan"],
+    mark: "340",
+    percentage: "85%",
+    rank: "9",
+    attendance: "160"
+  }
+  // കൂടുതൽ students ഇവിടെ ചേർക്കാം
+];
