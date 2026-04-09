b4f48fba34cb938db8a1a763d733350c04b49675
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-09 06:53:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,24 +1,17 @@
-// Student data
 const students = [
   {
     name: "Muhammed Fawzan",
     class: "8",
     roll: "101",
-    subjects: ["Fiqh", "Thareeq", "Duroos", "Lisan"],
-    mark: "350",
+    subjects: [
+      { name: "Fiqh", total: 50, mark: 45 },
+      { name: "Thareeq", total: 50, mark: 40 },
+      { name: "Duroos", total: 50, mark: 42 },
+      { name: "Lisan", total: 50, mark: 43 }
+    ],
+    total: 170,
     percentage: "87.5%",
     rank: "8",
     attendance: "163"
-  },
-  {
-    name: "Amina Sulaiman",
-    class: "8",
-    roll: "102",
-    subjects: ["Fiqh", "Thareeq", "Duroos", "Lisan"],
-    mark: "340",
-    percentage: "85%",
-    rank: "9",
-    attendance: "160"
   }
-  // കൂടുതൽ students ഇവിടെ ചേർക്കാം
 ];
