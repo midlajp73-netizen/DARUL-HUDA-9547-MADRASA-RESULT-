7eda7be792ec645be454d4c804417c35a43b679f
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-10 09:04:36 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,16 +2,16 @@ const students = [
   {
     name: "Muhammed Fawzan",
     class: "8",
-    roll: "801",   
+    roll: "801",
     subjects: [
       { name: "Fiqh", total: 50, mark: 45 },
       { name: "Thareeq", total: 50, mark: 40 },
       { name: "Duroos", total: 50, mark: 42 },
       { name: "Lisan", total: 50, mark: 43 }
     ],
-    total: 170,
-    percentage: "87.5%",
+    total: 170, // Correct total (45+40+42+43 = 170)
+    percentage: "85%", // Correct percentage
     rank: "8",
     attendance: "163"
-  },
-    ]
+  }
+];
