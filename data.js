81f32214793fab840c4b5fe6ea6e1b0f74c8af17
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-11 15:35:15 +0530

    Update data.js

--- data.js
+++ data.js
@@ -11,7 +11,24 @@ const students = [
     ],
     total: 170,
     percentage: 85,
-    rank: 8,
+    rank: 1,
     attendance: 163
+  },
+
+  // 👇 NEW STUDENT ADDED
+  {
+    name: "Abdullah",
+    class: "8",
+    roll: "802",
+    subjects: [
+      { name: "Fiqh", total: 50, mark: 38 },
+      { name: "Thareeq", total: 50, mark: 42 },
+      { name: "Duroos", total: 50, mark: 40 },
+      { name: "Lisan", total: 50, mark: 39 }
+    ],
+    total: 159,
+    percentage: 79.5,
+    rank: 2,
+    attendance: 160
   }
 ];
