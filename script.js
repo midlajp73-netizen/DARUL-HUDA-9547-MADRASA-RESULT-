fe4cfe19e9be66eae24b45dce883fd6c7b77fb1b
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-11 07:32:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-console.log(students);
 function searchResult() {
   const rollNumber = document.getElementById("rollInput").value.trim();
+
   const result = students.find(student => student.roll == rollNumber);
 
   if (result) {
+
+    // Fill data
     document.getElementById("name").innerText = result.name;
     document.getElementById("class").innerText = result.class;
     document.getElementById("roll").innerText = result.roll;
@@ -14,22 +16,19 @@ function searchResult() {
     let isFail = false;
 
     result.subjects.forEach(sub => {
-      if (sub.mark < 18) {
-        isFail = true;
-      }
+      if (sub.mark < 18) isFail = true;
 
-      const row = `
+      table.innerHTML += `
         <tr>
           <td>${sub.name}</td>
           <td>${sub.total}</td>
           <td>${sub.mark}</td>
         </tr>
       `;
-      table.innerHTML += row;
     });
 
     document.getElementById("total").innerText = result.total;
-    document.getElementById("percentage").innerText = result.percentage;
+    document.getElementById("percentage").innerText = result.percentage + "%";
     document.getElementById("rank").innerText = result.rank;
     document.getElementById("attendance").innerText = result.attendance;
 
@@ -43,13 +42,13 @@ function searchResult() {
       statusElement.style.color = "green";
     }
 
+    // 🔥 VERY IMPORTANT (SHOW RESULT)
     document.getElementById("resultSection").style.display = "block";
 
   } else {
     alert("Result not found!");
   }
 }
-
 
 // ✅ PDF FUNCTION (OUTSIDE)
 <script>
