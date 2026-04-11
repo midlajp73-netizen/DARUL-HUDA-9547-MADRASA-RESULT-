f32185db4b4d5cc25ebf57954a8fc5e8e5e0b235
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-11 21:06:27 +0530

    Update data.js

--- data.js
+++ data.js
@@ -93,5 +93,72 @@ const students = [
     percentage: 50.5,
     rank: 6,
     attendance: 84
+  },
+
+  {
+    name: "Muhammed Fawzan",
+    class: "8",
+    roll: "801",
+    subjects: [
+      { name: "Fiqh", total: 50, mark: 27 },
+      { name: "Thareeq", total: 50, mark: 29 },
+      { name: "Duroos", total: 50, mark: 26 },
+      { name: "Lisan", total: 50, mark: 33 }
+    ],
+    total: 115,
+    percentage: 57.5,
+    rank: 1,
+    attendance: 186
+  },
+
+  {
+    name: "Muhammed Fawzan",
+    class: "8",
+    roll: "801",
+    subjects: [
+      { name: "Fiqh", total: 50, mark: 27 },
+      { name: "Thareeq", total: 50, mark: 29 },
+      { name: "Duroos", total: 50, mark: 26 },
+      { name: "Lisan", total: 50, mark: 33 }
+    ],
+    total: 115,
+    percentage: 57.5,
+    rank: 1,
+    attendance: 186
+  },
+
+  {
+    name: "Muhammed Fawzan",
+    class: "8",
+    roll: "801",
+    subjects: [
+      { name: "Fiqh", total: 50, mark: 27 },
+      { name: "Thareeq", total: 50, mark: 29 },
+      { name: "Duroos", total: 50, mark: 26 },
+      { name: "Lisan", total: 50, mark: 33 }
+    ],
+    total: 115,
+    percentage: 57.5,
+    rank: 1,
+    attendance: 186
+  },
+
+  {
+    name: "Muhammed Fawzan",
+    class: "8",
+    roll: "801",
+    subjects: [
+      { name: "Fiqh", total: 50, mark: 27 },
+      { name: "Thareeq", total: 50, mark: 29 },
+      { name: "Duroos", total: 50, mark: 26 },
+      { name: "Lisan", total: 50, mark: 33 }
+    ],
+    total: 115,
+    percentage: 57.5,
+    rank: 1,
+    attendance: 186
   }
+
 ];
+
+  
